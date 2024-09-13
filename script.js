10a2d8f279d33fa8e2dfc0a7287f4e2d73fed826
Kunnikar B <kunnikarbcreative@gmail.com>
2024-09-13 14:44:45 +0200

    Added Typewriter and updated form

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter('#recipe', {
+    strings: 'Your recipe is being generated. Please wait a moment. 🧑🏻‍🍳',
+    autoStart: true,
+    delay: 40,
+    cursor: ' 🇹🇭',
+  });
+}
+
+let recipeFormElement = document.querySelector('#recipe-form');
+recipeFormElement.addEventListener('submit', generateRecipe);
 
