c94157e418b0ac84ae4482aa72bd46c1c5a00446
Kunnikar B <kunnikarbcreative@gmail.com>
2024-09-13 17:15:51 +0200

    Modified script.js

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function generateRecipe(response) {
   new Typewriter('#recipe', {
-    strings: 'Your recipe is being generated. Please wait a moment. 🧑🏻‍🍳',
+    strings: response.data.answer,
     autoStart: true,
-    delay: 40,
+    delay: 1,
     cursor: ' 🇹🇭',
   });
 }
 
-let recipeFormElement = document.querySelector('#recipe-form');
-recipeFormElement.addEventListener('submit', generateRecipe);
+function getRecipe(event) {
+  event.preventDefault();
 
+  let apiKey = '6654fa001cd5343d304f9bt068o34b25';
+  let context =
+    'Focusing on user input form and straightforward responses. Provide concise answers. The recipe must be provided in HTML format. Example: <p>this is a recipe</p> but do not show the word HTML';
+  let prompt = `Generate a Thai recipe for ${
+    document.querySelector('#recipe-name').value
+  }`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector('#recipe');
+  recipeElement.classList.remove('hidden');
+  recipeElement.innerHTML = `<div class="generating"> AI Generating a recipe for ${
+    document.querySelector('#recipe-name').value
+  } please wait 🍲 </div>`;
+
+  axios.get(apiUrl).then(generateRecipe);
+}
+
+let recipeButton = document.querySelector('#recipe-form ');
+recipeButton.addEventListener('submit', getRecipe);
 
